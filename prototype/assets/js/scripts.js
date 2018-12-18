f654f08ba20a50b3e569f7a142b40ea76d14134c
dervism <dervis4@gmail.com>
2018-12-18 12:16:43 +0100

    Refactor code, add comments and fix minor issues.

--- prototype/assets/js/scripts.js
+++ prototype/assets/js/scripts.js
@@ -2,36 +2,126 @@ moment.locale('nb');
 let current = 0;
 let pastEvents = undefined;
 
+/**
+ * Type that makes it easier to construct HTMLElement objects.
+ * This is used to dynamically create the OSWA photo matrix.
+ */
 const html = {
-    gid: el => document.getElementById(el),
+    /**
+     * Short-hand function to get elements by id.
+     * @param el the element to get
+     * @returns {HTMLElement} the dom element
+     */
+    getId: el => document.getElementById(el),
 
-    el: el => document.createElement(el),
+    /**
+     * Creates new dom elements.
+     * @param el type of element to create
+     * @returns {HTMLElement} the created dom element
+     */
+    element: el => document.createElement(el),
 
-    // add class names
-    cn: (el, cn) => el.classList.add(cn),
+    /**
+     * Add a single class name (multiple names
+     * separated by white space are not allowed).
+     * @param el the dom element to update
+     * @param cn the class name to add
+     */
+    className: (el, cn) => el.classList.add(cn),
 
-    // element add, return child
-    ela: (el, ch) => {
+    /**
+     * Add a child element and return the child.
+     * @param el Parent dom element
+     * @param ch Child dom element
+     * @returns {HTMLElement} The added child element
+     */
+    elementChildAdd: (el, ch) => {
         el.appendChild(ch);
         return ch;
     },
 
-    // element add, return parent
-    elap: (el, ch) => {
+    /**
+     * Add a child element and return the parent.
+     * @param el Parent dom element
+     * @param ch Child dom element
+     * @returns {HTMLElement} The parent element
+     */
+    elementParentAdd: (el, ch) => {
         el.appendChild(ch);
         return el;
     },
 
-    // create element, add class names
-    elp: (e, p) => {
-        const element = html.el(e);
-        if (p && p.indexOf(' ') > -1) p.split(' ').forEach(c => html.cn(element, c));
-        else if (p && Array.isArray(p)) p.forEach(c => html.cn(element, c));
-        else if (p && p.indexOf(' ') === -1) html.cn(element, p);
+    /**
+     * Create a new element and add class names if any.
+     * @param el Ttype of element to create
+     * @param classnames The class names to add. Can be a white spaces separated string or an array.
+     * @return {*|HTMLElement}
+     */
+    elementClass: (el, classnames) => {
+        const element = html.element(el);
+        if (classnames && classnames.indexOf(' ') > -1) classnames.split(' ').forEach(c => html.className(element, c));
+        else if (classnames && Array.isArray(classnames)) classnames.forEach(c => html.className(element, c));
+        else if (classnames && classnames.indexOf(' ') === -1) html.className(element, classnames);
         return element;
     }
 };
 
+/**
+ * Makes a call to the Meetup.com API and returns a promise.
+ * The call uses signatures that cannot be changed.
+ * Goto https://secure.meetup.com/meetup_api/console/?path=/:urlname/events
+ * if you need to change the parameters of the call.
+ *
+ * @returns {PromiseLike<T|never>|Promise<T|never>|*}
+ */
+function getOSWAEvents() {
+    return getEvents("https://api.meetup.com/Oslo-Software-Architecture/events" + getFields() + getSigns());
+}
+
+/**
+ * See Meetup.com API docs for more
+ * information:
+ * https://www.meetup.com/meetup_api/docs/
+ *
+ * @returns {string} URL with field parameters combined.
+ */
+function getFields() {
+    const fields = [
+        "id",
+        "featured_photo",
+        "short_link", // A shortened link for the event on meetup.com
+        "rsvp_limit", // The number of "yes" RSVPS an event has capacity for
+        "how_to_find_us",
+        "event_hosts", // .name, .intro, .photo.photo_link
+        "featured", // Boolean indicator of whether or not a given event is featured,
+        "photo_album"
+    ];
+
+    const allFields = fields.reduce((p, n) => p + "," + n);
+    //console.log(allFields);
+    return "?&photo-host=public&&scroll=future_or_past&page=20&fields="+allFields;
+}
+
+/**
+ * Returns a Promise that resolves when the HTTP call
+ * is successfully done.
+ *
+ * @param url
+ * @returns {PromiseLike<T | never> | Promise<T | never> | *}
+ */
+function getEvents(url) {
+    return $.ajax({
+        url: url,
+        dataType: 'jsonp'}).then(result => {
+        return result.data;
+    })
+}
+
+/**
+ * Loads, parses and renders events from meetup.com in a Promise-chain.
+ *
+ * @returns {PromiseLike<any | never> | Promise<any | never>}
+ */
 function loadMeetups() {
     return getOSWAEvents()
         .then(events => {
@@ -39,6 +129,13 @@ function loadMeetups() {
         });
 }
 
+/**
+ * Creates a Promise around the rendering that resolves
+ * only when the templating process is complete.
+ *
+ * @param data JSON array containing events.
+ * @returns {Promise<any>} Resolves with empty function when done.
+ */
 function parseEvents(data) {
   if (data && Array.isArray(data)) {
     return new Promise((resolve, reject) => {
@@ -55,6 +152,12 @@ function parseEvents(data) {
   }
 }
 
+/**
+ * Reads the JSON data from meetup.com
+ * and prepares the results for rendering.
+ *
+ * @param data JSON data from meetup.com
+ */
 function templateEvents(data) {
   if (!data || data.length === 0) return;
 
@@ -93,6 +196,10 @@ document.querySelector( "#showMoreBtn").addEventListener('click', () => {
     showMore()
 });
 
+/**
+ * Renders an increasing amount of events with every click
+ * to the #showMoreBtn button.
+ */
 function showMore() {
     current += 3;
     let end = current;
@@ -102,14 +209,19 @@ function showMore() {
         document.querySelector( "#showMoreBtn").classList.add("disabled");
     }
 
+    // make shallow copy of the event array and then render them separately.
     const events = pastEvents.slice(0, end);
     render("#pastTemplate", "pastMeetupList", events);
 }
 
+/**
+ * Creates a photo matrix that follows the DOM-structure set by the
+ * Prototype template by Pixelarity.com.
+ */
 function loadPhotoAlbums() {
-    const grid = html.ela(
-        html.elp('div', 'box alt'),
-        html.elp('div', 'row gtr-uniform')
+    const grid = html.elementChildAdd(
+        html.elementClass('div', 'box alt'),
+        html.elementClass('div', 'row gtr-uniform')
     );
 
     const imageUrls = [];
@@ -124,26 +236,29 @@ function loadPhotoAlbums() {
     });
 
     shuffleArray(imageUrls).forEach(sample => {
-        let gridCell = html.elp('div', 'col-3');
-        const img = html.ela(
-            html.ela(gridCell, html.elp('span', 'image fit')),
-            html.el('img')
+        let gridCell = html.elementClass('div', 'col-3');
+        const img = html.elementChildAdd(
+            html.elementChildAdd(gridCell, html.elementClass('span', 'image fit')),
+            html.element('img')
         );
         img.setAttribute('src', sample.photo_link);
         img.setAttribute('alt', sample.title);
-        html.ela(grid, gridCell);
+        html.elementChildAdd(grid, gridCell);
     });
 
-    const container = html.gid('communityPhotosGrid');
-    html.ela(container, grid);
+    const container = html.getId('communityPhotosGrid');
+    html.elementChildAdd(container, grid);
 }
 
+/**
+ * Using Durstenfeld shuffle algorithm.
+ */
 function shuffleArray(arr) {
     const array = arr.slice();
 
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
@@ -152,26 +267,26 @@ function shuffleArray(arr) {
 }
 
 function loadPhotoAlbumCircles() {
-    let imageCircles = html.elp('div', 'image-circles');
+    let imageCircles = html.elementClass('div', 'image-circles');
 
     for (let i = 0; i < 3; i++) {
-        const imageContainer = html.ela(
+        const imageContainer = html.elementChildAdd(
             imageCircles,
-            html.elp('div', 'images')
+            html.elementClass('div', 'images')
         );
 
         for (let j = 0; j < 3; j++) {
-            let gridCell = html.elp('span', 'image');
-            const img = html.el('img');
+            let gridCell = html.elementClass('span', 'image');
+            const img = html.element('img');
             img.setAttribute('src', 'images/oswa-logo-s.jpeg');
             img.setAttribute('alt', 'event photo');
 
-            html.ela(imageContainer, html.elap(gridCell, img));
+            html.elementChildAdd(imageContainer, html.elementParentAdd(gridCell, img));
         }
     }
 
-    const container = html.gid('communityPhotosGrid');
-    html.ela(container, imageCircles);
+    const container = html.getId('communityPhotosGrid');
+    html.elementChildAdd(container, imageCircles);
 }
 
 function render(templateName, element, data) {
@@ -180,42 +295,6 @@ function render(templateName, element, data) {
     document
         .getElementById(element)
         .innerHTML = template.render({events: data});
-}
-
-function getOSWAEvents() {
-  return getEvents("https://api.meetup.com/Oslo-Software-Architecture/events" + getFields() + getSigns());
-}
-
-function getEvents(url) {
-    return $.ajax({
-        url: url,
-        dataType: 'jsonp'}).then(result => {
-        return result.data;
-    })
-}
-
-/**
- * See Meetup.com API docs for more
- * information:
- * https://www.meetup.com/meetup_api/docs/
- *
- * @returns {string} URL with field parameters combined.
- */
-function getFields() {
-  const fields = [
-      "id",
-      "featured_photo",
-      "short_link", // A shortened link for the event on meetup.com
-      "rsvp_limit", // The number of "yes" RSVPS an event has capacity for
-      "how_to_find_us",
-      "event_hosts", // .name, .intro, .photo.photo_link, .join_date,  .photo.thumb_link, .photo.highres_link
-      "featured", // Boolean indicator of whether or not a given event is featured,
-      "photo_album"
-  ];
-
-  const allFields = fields.reduce((p, n) => p + "," + n);
-  //console.log(allFields);
-  return "?&photo-host=public&&scroll=future_or_past&page=20&fields="+allFields;
 }
 
 function recap(response) {
@@ -275,6 +354,6 @@ function getSigns() {
     return "&sig_id=35117512&sig=9ed5bc0ed689f88e22fee868a4fa215fddd4a35f" // 10 = d42fecb23546e55dc7f0b799b2191371812462c5 // 20 = 9ed5bc0ed689f88e22fee868a4fa215fddd4a35f
 }
 
-loadMeetups().then(result => {
+loadMeetups().then(() => {
     loadPhotoAlbums();
 });
